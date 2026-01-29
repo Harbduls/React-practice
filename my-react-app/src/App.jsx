c3b37fed3cfa9ec945a403344d450e90f926b5a4
usman abdulbasit <yasinbasit20@gmail.com>
2026-01-29 05:06:42 +0100

    Add profile cards feature

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Card from "./Card"
+import './index.css'
+
+export function App() {
+  const profiles = [
+    {
+      id: 1,
+      name: "Usman Abdulbasit",
+      title: "Frontend developer",
+      bio: "I like to work with different frontend technologies and play video games."
+    },
+
+    {
+        id : 2,
+        name : "Tiffany",
+        title : "Engineering manager",
+        bio : "I have worked in tech for 15 years and love to help people grow in this industry."
+    },
+    {
+      id : 3,
+      name : "Doug",
+      title : "Backend developer",
+      bio : " I have been a software developer for over 20 years and I love working with Go and Rust."  
+    }
+ ];
+  return (
+    <div className="flex-container">
+      {profiles.map((profile) => (
+        <Card
+          key={profile.id}
+          name={profile.name}
+          title={profile.title}
+          bio={profile.bio}
+        />
+      ))}
+    </div>
+  );
+}
